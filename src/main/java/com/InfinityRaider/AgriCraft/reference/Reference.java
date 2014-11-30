425b45871c95da2c2be54136f7c4601f90f6dc69
InfinityRaider <InfinityRaider@users.noreply.github.com>
2014-11-30 22:29:17 +0100

    version 0.0.9

--- src/main/java/com/InfinityRaider/AgriCraft/reference/Reference.java
+++ src/main/java/com/InfinityRaider/AgriCraft/reference/Reference.java
@@ -3,7 +3,7 @@ package com.InfinityRaider.AgriCraft.reference;
 public final class Reference {
     public static final String MOD_NAME = "AgriCraft";
     public static final String MOD_ID = "AgriCraft";
-    public static final String VERSION = "1.7.10-0.0.8a";
+    public static final String VERSION = "1.7.10-0.0.9";
     public static final String AUTHOR = "InfinityRaider";
     public static boolean debug;
     public static final String CLIENT_PROXY_CLASS = "com.InfinityRaider.AgriCraft.proxy.ClientProxy";
