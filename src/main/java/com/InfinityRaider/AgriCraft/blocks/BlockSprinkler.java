c4b96e4984760076b5f1a9816354f0f7993cd6b1
InfinityRaider <InfinityRaider@users.noreply.github.com>
2014-12-04 21:33:28 +0100

    Sprinkler breaking logic

--- src/main/java/com/InfinityRaider/AgriCraft/blocks/BlockSprinkler.java
+++ src/main/java/com/InfinityRaider/AgriCraft/blocks/BlockSprinkler.java
@@ -1,10 +1,15 @@
 package com.InfinityRaider.AgriCraft.blocks;
 
+import com.InfinityRaider.AgriCraft.init.Items;
 import com.InfinityRaider.AgriCraft.tileentity.TileEntitySprinkler;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
@@ -22,6 +27,38 @@ public class BlockSprinkler extends BlockContainer {
     @Override
     public boolean isReplaceable(IBlockAccess world, int x, int y, int z) {
         return false;
+    }
+
+    @Override
+    public void dropBlockAsItemWithChance(World world, int x, int y, int z, int meta, float f, int i) {
+        if(!world.isRemote) {
+            ItemStack drop = new ItemStack(Items.sprinkler, 1);
+            this.dropBlockAsItem(world, x, y, z, drop);
+        }
+    }
+
+    @Override
+    public void onNeighborBlockChange(World world, int x, int y, int z, Block block) {
+        //check if crops can stay
+        if(!this.canBlockStay(world,x,y,z)) {
+            //the crop will be destroyed
+            this.dropBlockAsItem(world, x, y, z, world.getBlockMetadata(x, y, z), 0);
+            world.setBlockToAir(x,y,z);
+            world.removeTileEntity(x,y,z);
+        }
+    }
+
+    //see if the block can stay
+    @Override
+    public boolean canBlockStay(World world, int x, int y, int z) {
+        Block sprinkler = world.getBlock(x,y+1,z);
+        return (sprinkler==com.InfinityRaider.AgriCraft.init.Blocks.blockSprinkler);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public Item getItem(World world, int x, int y, int z) {
+        return Items.sprinkler;
     }
 
     //rendering stuff
