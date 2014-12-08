e442a6a7fa31cf2b542c38b9284fd96e4d23aa12
InfinityRaider <InfinityRaider@users.noreply.github.com>
2014-12-08 21:39:33 +0100

    Railcraft Hidden Blocks no longer prevent crops from being placed

--- src/main/java/com/InfinityRaider/AgriCraft/items/ItemCrop.java
+++ src/main/java/com/InfinityRaider/AgriCraft/items/ItemCrop.java
@@ -2,6 +2,7 @@ package com.InfinityRaider.AgriCraft.items;
 
 import com.InfinityRaider.AgriCraft.creativetab.AgriCraftTab;
 import com.InfinityRaider.AgriCraft.init.Blocks;
+import mods.railcraft.common.blocks.hidden.BlockHidden;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
@@ -20,7 +21,7 @@ public class ItemCrop extends ModItem {
     @Override
     public boolean onItemUseFirst(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
         if (!world.isRemote) {
-            if (world.getBlock(x, y, z) == net.minecraft.init.Blocks.farmland && world.getBlock(x, y + 1, z) == net.minecraft.init.Blocks.air && side == 1) {
+            if (world.getBlock(x, y, z)==net.minecraft.init.Blocks.farmland && (world.getBlock(x, y + 1, z)==net.minecraft.init.Blocks.air || world.getBlock(x, y + 1, z)instanceof BlockHidden) && side == 1) {
                 world.setBlock(x, y + 1, z, Blocks.blockCrop);
                 stack.stackSize = player.capabilities.isCreativeMode ? stack.stackSize : stack.stackSize - 1;
                 return false;
