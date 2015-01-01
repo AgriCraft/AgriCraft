83e748ca9845ed89a872a0e294485f4b80954f28
InfinityRaider <InfinityRaider@users.noreply.github.com>
2015-01-01 14:14:47 +0100

    parsing method rewrite

--- src/main/java/com/InfinityRaider/AgriCraft/utility/IOHelper.java
+++ src/main/java/com/InfinityRaider/AgriCraft/utility/IOHelper.java
@@ -10,6 +10,7 @@ import net.minecraft.item.ItemSeeds;
 import net.minecraft.item.ItemStack;
 
 import java.io.*;
+import java.util.ArrayList;
 
 //class containing the default mutations for each supported mod
 public abstract class IOHelper {
@@ -124,32 +125,26 @@ public abstract class IOHelper {
     //turns the raw data string into an array (each array element is a line from the string)
     public static String[] getLinesArrayFromData(String input) {
         int count = 0;
-        for(int i=0;i<input.length();i++) {
-            if(input.charAt(i)=='\n') {
+        String unprocessed = input;
+        for (int i=0;i<unprocessed.length();i++) {
+            if (unprocessed.charAt(i) == '\n') {
                 count++;
             }
-            else if (input.charAt(i)=='#') {
-                count--;
-            }
         }
-        count = count>=0?count+1:count;  //for the last line
-        count = count<0?0:count;        //make sure it can't be negative
-        String[] data = new String[count];
-        if(input.length()>0) {
-            int start = 0;
-            int stop;
-            for (int i = 0; i < data.length; i++) {
-                stop = input.indexOf('\n', start);
-                while (start<input.length() && input.charAt(start) == '#' && start<input.length()) {
-                    start = stop + 1;
-                    stop = input.indexOf('\n', start);
+        ArrayList<String> data = new ArrayList<String>();
+        if (unprocessed.length()>0) {
+            for (int i=0;i<count;i++) {
+                String line = (unprocessed.substring(0,unprocessed.indexOf('\n'))).trim();
+                if (line.length() > 0 && line.charAt(0) != '#') {
+                    data.add(line);
                 }
-                data[i] = i == data.length - 1 ? input.substring(start) : input.substring(start, stop);
-                start = stop + 1;
+                unprocessed = unprocessed.substring(unprocessed.indexOf('\n')+1);
             }
         }
-        return data;
-
+        if ((unprocessed.trim()).length()>0 && unprocessed.charAt(0)!='#') {
+            data.add(unprocessed.trim());
+        }
+        return data.toArray(new String[data.size()]);
     }
 
     public static String[] getCropData(String input) {
