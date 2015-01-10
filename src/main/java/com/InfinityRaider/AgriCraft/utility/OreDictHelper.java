15ed421d6230d43fb467c073211a81689e3b6571
Simon Zuend <simon.zuend@gmx.net>
2015-01-11 00:55:41 +0100

    Refactors OreDictHelper.

--- src/main/java/com/InfinityRaider/AgriCraft/utility/OreDictHelper.java
+++ src/main/java/com/InfinityRaider/AgriCraft/utility/OreDictHelper.java
@@ -3,15 +3,45 @@ package com.InfinityRaider.AgriCraft.utility;
 import com.InfinityRaider.AgriCraft.init.Items;
 import com.InfinityRaider.AgriCraft.reference.Names;
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 
-import java.util.ArrayList;
+import java.util.*;
 
 //lots of ctrl+c & ctrl+v (read: bad programming) in this class, viewer discretion is advised.
 //not for sensitive programmer eyes
 public abstract class OreDictHelper {
+
+    private static final Map<String, Block> oreBlocks = new HashMap<String, Block>();
+    private static final Map<String, Item> oreItems = new HashMap<String, Item>();
+    private static final Map<String, Integer> oreMetaData = new HashMap<String, Integer>();
+
+    private static final List<String> oreNames;
+    private static final List<String> nuggetNames;
+
+    static {
+        oreNames = Arrays.asList(Names.Ores.oreCopper, Names.Ores.oreTin, Names.Ores.oreLead,
+                Names.Ores.oreSilver, Names.Ores.oreAluminum, Names.Ores.oreNickel,
+                Names.Ores.orePlatinum, Names.Ores.oreOsmium);
+
+        nuggetNames = Arrays.asList(Names.Nuggets.nuggetDiamond, Names.Nuggets.nuggetEmerald,
+                Names.Nuggets.nuggetIron, Names.Nuggets.nuggetCopper, Names.Nuggets.nuggetTin,
+                Names.Nuggets.nuggetLead, Names.Nuggets.nuggetSilver, Names.Nuggets.nuggetAluminum,
+                Names.Nuggets.nuggetNickel, Names.Nuggets.nuggetPlatinum, Names.Nuggets.nuggetOsmium);
+    }
+
+    public static Block getOreBlockForName(String name) {
+        return oreBlocks.get(name);
+    }
+
+    public static int getOreMetaDataForName(String name) {
+        return oreMetaData.get(name);
+    }
+
+    // TODO: Get rid of instance fields!! Access blocks and metadata only through the maps (via access methods)
+
     public static Block oreCopper;
     public static Block oreTin;
     public static Block oreLead;
@@ -75,170 +105,97 @@ public abstract class OreDictHelper {
 
     //finds what ores and nuggets are already registered in the ore dictionary
     public static void getRegisteredOres() {
-        String[] oreDictNames = OreDictionary.getOreNames();
-        for (String ore:oreDictNames) {
-            if(ore.equals(Names.Ores.oreCopper)) {
-                boolean flag = true;
-                int i = 0;
-                while(flag && i<OreDictionary.getOres(ore).size()) {
-                    if(OreDictionary.getOres(ore).get(i).getItem() instanceof  ItemBlock) {
-                        oreCopper = ((ItemBlock) OreDictionary.getOres(ore).get(i).getItem()).field_150939_a;
-                        oreCopperMeta = OreDictionary.getOres(ore).get(i).getItemDamage();
-                        flag = false;
-                    }
-                    i++;
-                }
-            }
-            else if(ore.equals(Names.Ores.oreTin)) {
-                boolean flag = true;
-                int i = 0;
-                while(flag && i<OreDictionary.getOres(ore).size()) {
-                    if(OreDictionary.getOres(ore).get(i).getItem() instanceof  ItemBlock) {
-                        oreTin = ((ItemBlock) OreDictionary.getOres(ore).get(i).getItem()).field_150939_a;
-                        oreTinMeta = OreDictionary.getOres(ore).get(i).getItemDamage();
-                        flag = false;
-                    }
-                    i++;
-                }
-            }
-            else if(ore.equals(Names.Ores.oreLead)) {
-                boolean flag = true;
-                int i = 0;
-                while(flag && i<OreDictionary.getOres(ore).size()) {
-                    if(OreDictionary.getOres(ore).get(i).getItem() instanceof  ItemBlock) {
-                        oreLead = ((ItemBlock) OreDictionary.getOres(ore).get(i).getItem()).field_150939_a;
-                        oreLeadMeta = OreDictionary.getOres(ore).get(i).getItemDamage();
-                        flag = false;
-                    }
-                    i++;
-                }
-            }
-            else if(ore.equals(Names.Ores.oreSilver)) {
-                boolean flag = true;
-                int i = 0;
-                while(flag && i<OreDictionary.getOres(ore).size()) {
-                    if(OreDictionary.getOres(ore).get(i).getItem() instanceof  ItemBlock) {
-                        oreSilver = ((ItemBlock) OreDictionary.getOres(ore).get(i).getItem()).field_150939_a;
-                        oreSilverMeta = OreDictionary.getOres(ore).get(i).getItemDamage();
-                        flag = false;
-                    }
-                    i++;
-                }
-            }
-            else if(ore.equals(Names.Ores.oreAluminum)) {
-                boolean flag = true;
-                int i = 0;
-                while(flag && i<OreDictionary.getOres(ore).size()) {
-                    if(OreDictionary.getOres(ore).get(i).getItem() instanceof  ItemBlock) {
-                        oreAluminum = ((ItemBlock) OreDictionary.getOres(ore).get(i).getItem()).field_150939_a;
-                        oreAluminumMeta = OreDictionary.getOres(ore).get(i).getItemDamage();
-                        flag = false;
-                    }
-                    i++;
-                }
-            }
-            else if(ore.equals(Names.Ores.oreNickel)) {
-                boolean flag = true;
-                int i = 0;
-                while(flag && i<OreDictionary.getOres(ore).size()) {
-                    if(OreDictionary.getOres(ore).get(i).getItem() instanceof  ItemBlock) {
-                        oreNickel = ((ItemBlock) OreDictionary.getOres(ore).get(i).getItem()).field_150939_a;
-                        oreNickelMeta = OreDictionary.getOres(ore).get(i).getItemDamage();
-                        flag = false;
-                    }
-                    i++;
-                }
-            }
-            else if(ore.equals(Names.Ores.orePlatinum)) {
-                boolean flag = true;
-                int i = 0;
-                while(flag && i<OreDictionary.getOres(ore).size()) {
-                    if(OreDictionary.getOres(ore).get(i).getItem() instanceof  ItemBlock) {
-                        orePlatinum= ((ItemBlock) OreDictionary.getOres(ore).get(i).getItem()).field_150939_a;
-                        orePlatinumMeta = OreDictionary.getOres(ore).get(i).getItemDamage();
-                        flag = false;
-                    }
-                    i++;
-                }
-            }
-            else if(ore.equals(Names.Ores.oreOsmium)) {
-                boolean flag = true;
-                int i = 0;
-                while(flag && i<OreDictionary.getOres(ore).size()) {
-                    if(OreDictionary.getOres(ore).get(i).getItem() instanceof  ItemBlock) {
-                        oreOsmium = ((ItemBlock) OreDictionary.getOres(ore).get(i).getItem()).field_150939_a;
-                        oreOsmiumMeta = OreDictionary.getOres(ore).get(i).getItemDamage();
-                        flag = false;
-                    }
-                    i++;
-                }
-            }
-            else if(ore.equals(Names.Nuggets.nuggetDiamond)) {
-                if(OreDictionary.getOres(ore).size()>0) {
-                    Items.nuggetDiamond = OreDictionary.getOres(ore).get(0).getItem();
-                    Items.nuggetDiamondMeta = OreDictionary.getOres(ore).get(0).getItemDamage();
-                }
-            }
-            else if(ore.equals(Names.Nuggets.nuggetEmerald)) {
-                if(OreDictionary.getOres(ore).size()>0) {
-                    Items.nuggetEmerald = OreDictionary.getOres(ore).get(0).getItem();
-                    Items.nuggetEmeraldMeta = OreDictionary.getOres(ore).get(0).getItemDamage();
-                }
-            }
-            else if(ore.equals(Names.Nuggets.nuggetIron)) {
-                if(OreDictionary.getOres(ore).size()>0) {
-                    Items.nuggetIron = OreDictionary.getOres(ore).get(0).getItem();
-                    Items.nuggetIronMeta = OreDictionary.getOres(ore).get(0).getItemDamage();
-                }
-            }
-            else if(ore.equals(Names.Nuggets.nuggetCopper)) {
-                if(OreDictionary.getOres(ore).size()>0) {
-                    Items.nuggetCopper = OreDictionary.getOres(ore).get(0).getItem();
-                    Items.nuggetCopperMeta = OreDictionary.getOres(ore).get(0).getItemDamage();
-                }
-            }
-            else if(ore.equals(Names.Nuggets.nuggetTin)) {
-                if(OreDictionary.getOres(ore).size()>0) {
-                    Items.nuggetTin = OreDictionary.getOres(ore).get(0).getItem();
-                    Items.nuggetTinMeta = OreDictionary.getOres(ore).get(0).getItemDamage();
-                }
-            }
-            else if(ore.equals(Names.Nuggets.nuggetLead)) {
-                if(OreDictionary.getOres(ore).size()>0) {
-                    Items.nuggetLead = OreDictionary.getOres(ore).get(0).getItem();
-                    Items.nuggetLeadMeta = OreDictionary.getOres(ore).get(0).getItemDamage();
-                }
-            }
-            else if(ore.equals(Names.Nuggets.nuggetSilver)) {
-                if(OreDictionary.getOres(ore).size()>0) {
-                    Items.nuggetSilver = OreDictionary.getOres(ore).get(0).getItem();
-                    Items.nuggetSilverMeta = OreDictionary.getOres(ore).get(0).getItemDamage();
-                }
-            }
-            else if(ore.equals(Names.Nuggets.nuggetAluminum)) {
-                if(OreDictionary.getOres(ore).size()>0) {
-                    Items.nuggetAluminum = OreDictionary.getOres(ore).get(0).getItem();
-                    Items.nuggetAluminumMeta = OreDictionary.getOres(ore).get(0).getItemDamage();
-                }
-            }
-            else if(ore.equals(Names.Nuggets.nuggetNickel)) {
-                if(OreDictionary.getOres(ore).size()>0) {
-                    Items.nuggetNickel = OreDictionary.getOres(ore).get(0).getItem();
-                    Items.nuggetNickelMeta = OreDictionary.getOres(ore).get(0).getItemDamage();
-                }
-            }
-            else if(ore.equals(Names.Nuggets.nuggetPlatinum)) {
-                if(OreDictionary.getOres(ore).size()>0) {
-                    Items.nuggetPlatinum = OreDictionary.getOres(ore).get(0).getItem();
-                    Items.nuggetPlatinumMeta = OreDictionary.getOres(ore).get(0).getItemDamage();
-                }
-            }
-            else if(ore.equals(Names.Nuggets.nuggetOsmium)) {
-                if(OreDictionary.getOres(ore).size()>0) {
-                    Items.nuggetOsmium = OreDictionary.getOres(ore).get(0).getItem();
-                    Items.nuggetOsmiumMeta = OreDictionary.getOres(ore).get(0).getItemDamage();
+        registerOres();
+        registerNuggets();
+
+        initializeOreBlockFields();
+        initializeNuggetFields();
+    }
+
+    private static void registerOres() {
+        for (String oreName : oreNames) {
+            for (ItemStack itemStack : OreDictionary.getOres(oreName)) {
+                if (itemStack.getItem() instanceof ItemBlock) {
+                    ItemBlock block = (ItemBlock) itemStack.getItem();
+
+                    oreBlocks.put(oreName, block.field_150939_a);
+                    oreMetaData.put(oreName, itemStack.getItemDamage());
+                    break;
                 }
             }
         }
+    }
+
+    private static void registerNuggets() {
+        for (String nuggetName : nuggetNames) {
+            List<ItemStack> nuggets = OreDictionary.getOres(nuggetName);
+            if (!nuggets.isEmpty()) {
+                Item nugget = nuggets.get(0).getItem();
+
+                oreItems.put(nuggetName, nugget);
+                oreMetaData.put(nuggetName, nuggets.get(0).getItemDamage());
+            }
+        }
+    }
+
+    private static void initializeOreBlockFields() {
+        oreCopper = oreBlocks.get(Names.Ores.oreCopper);
+        if (oreCopper != null) oreCopperMeta = oreMetaData.get(Names.Ores.oreCopper);
+
+        oreTin = oreBlocks.get(Names.Ores.oreTin);
+        if (oreTin != null) oreTinMeta = oreMetaData.get(Names.Ores.oreTin);
+
+        oreLead = oreBlocks.get(Names.Ores.oreLead);
+        if (oreLead != null) oreLeadMeta = oreMetaData.get(Names.Ores.oreLead);
+
+        oreSilver = oreBlocks.get(Names.Ores.oreSilver);
+        if (oreSilver != null) oreSilverMeta = oreMetaData.get(Names.Ores.oreSilver);
+
+        oreAluminum = oreBlocks.get(Names.Ores.oreAluminum);
+        if (oreAluminum != null) oreAluminumMeta = oreMetaData.get(Names.Ores.oreAluminum);
+
+        oreNickel = oreBlocks.get(Names.Ores.oreNickel);
+        if (oreNickel != null) oreNickelMeta = oreMetaData.get(Names.Ores.oreNickel);
+
+        orePlatinum = oreBlocks.get(Names.Ores.orePlatinum);
+        if (orePlatinum != null) orePlatinumMeta = oreMetaData.get(Names.Ores.orePlatinum);
+
+        oreOsmium = oreBlocks.get(Names.Ores.oreOsmium);
+        if (oreOsmium != null) oreOsmiumMeta = oreMetaData.get(Names.Ores.oreOsmium);
+    }
+
+    private static void initializeNuggetFields() {
+        Items.nuggetDiamond = oreItems.get(Names.Nuggets.nuggetDiamond);
+        if (Items.nuggetDiamond != null) Items.nuggetDiamondMeta = oreMetaData.get(Names.Nuggets.nuggetDiamond);
+
+        Items.nuggetEmerald = oreItems.get(Names.Nuggets.nuggetEmerald);
+        if (Items.nuggetEmerald != null) Items.nuggetEmeraldMeta = oreMetaData.get(Names.Nuggets.nuggetEmerald);
+
+        Items.nuggetIron = oreItems.get(Names.Nuggets.nuggetIron);
+        if (Items.nuggetIron != null) Items.nuggetIronMeta = oreMetaData.get(Names.Nuggets.nuggetIron);
+
+        Items.nuggetCopper = oreItems.get(Names.Nuggets.nuggetCopper);
+        if (Items.nuggetCopper != null) Items.nuggetCopperMeta = oreMetaData.get(Names.Nuggets.nuggetCopper);
+
+        Items.nuggetTin = oreItems.get(Names.Nuggets.nuggetTin);
+        if (Items.nuggetTin != null) Items.nuggetTinMeta = oreMetaData.get(Names.Nuggets.nuggetTin);
+
+        Items.nuggetLead = oreItems.get(Names.Nuggets.nuggetLead);
+        if (Items.nuggetLead != null) Items.nuggetLeadMeta = oreMetaData.get(Names.Nuggets.nuggetLead);
+
+        Items.nuggetSilver = oreItems.get(Names.Nuggets.nuggetSilver);
+        if (Items.nuggetSilver != null) Items.nuggetSilverMeta = oreMetaData.get(Names.Nuggets.nuggetSilver);
+
+        Items.nuggetAluminum = oreItems.get(Names.Nuggets.nuggetAluminum);
+        if (Items.nuggetAluminum != null) Items.nuggetAluminumMeta = oreMetaData.get(Names.Nuggets.nuggetAluminum);
+
+        Items.nuggetNickel = oreItems.get(Names.Nuggets.nuggetNickel);
+        if (Items.nuggetNickel != null) Items.nuggetNickelMeta = oreMetaData.get(Names.Nuggets.nuggetNickel);
+
+        Items.nuggetEmerald = oreItems.get(Names.Nuggets.nuggetEmerald);
+        if (Items.nuggetPlatinum != null) Items.nuggetEmeraldMeta = oreMetaData.get(Names.Nuggets.nuggetEmerald);
+        
+        Items.nuggetOsmium = oreItems.get(Names.Nuggets.nuggetOsmium);
+        if (Items.nuggetOsmium != null) Items.nuggetOsmiumMeta = oreMetaData.get(Names.Nuggets.nuggetOsmium);
     }
 }
