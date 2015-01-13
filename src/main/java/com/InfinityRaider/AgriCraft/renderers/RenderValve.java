fb74d63f479c7a1131e472520701f0d9e6ca5398
InfinityRaider <InfinityRaider@users.noreply.github.com>
2015-01-13 19:43:49 +0100

    redundant interface

--- src/main/java/com/InfinityRaider/AgriCraft/renderers/RenderValve.java
+++ src/main/java/com/InfinityRaider/AgriCraft/renderers/RenderValve.java
@@ -16,7 +16,7 @@ import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
 
 
-public class RenderValve extends RenderChannel implements ISimpleBlockRenderingHandler {
+public class RenderValve extends RenderChannel {
 
     @Override
     public void renderInventoryBlock(Block block, int metadata, int modelId, RenderBlocks renderer) {
