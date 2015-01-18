5900f5a06e1a3a811f2f5f99316554877b080eca
InfinityRaider <InfinityRaider@users.noreply.github.com>
2015-01-18 14:39:23 +0100

    draw gui

--- src/main/java/com/InfinityRaider/AgriCraft/gui/GuiSeedStorage.java
+++ src/main/java/com/InfinityRaider/AgriCraft/gui/GuiSeedStorage.java
@@ -3,9 +3,16 @@ package com.InfinityRaider.AgriCraft.gui;
 import com.InfinityRaider.AgriCraft.container.ContainerSeedStorage;
 import com.InfinityRaider.AgriCraft.reference.Reference;
 import com.InfinityRaider.AgriCraft.tileentity.TileEntitySeedStorage;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.item.ItemSeeds;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class GuiSeedStorage extends GuiContainer {
     public static final ResourceLocation texture = new ResourceLocation(Reference.MOD_ID.toLowerCase(), "textures/gui/GuiSeedStorage.png");
@@ -18,9 +25,18 @@ public class GuiSeedStorage extends GuiContainer {
         this.ySize = 176;
     }
 
+    //draw foreground
+    @Override
+    public void drawGuiContainerForegroundLayer(int x, int y) {
+        GL11.glColor4f(1F, 1F, 1F, 1F);
+        HashMap<ItemSeeds, HashMap<Integer, ArrayList<ItemStack>>> entries = ((ContainerSeedStorage) this.inventorySlots).entries;
+    }
+
     @Override
     protected void drawGuiContainerBackgroundLayer(float opacity, int x, int y) {
-
+        GL11.glColor4f(1F, 1F, 1F, 1F);
+        Minecraft.getMinecraft().getTextureManager().bindTexture(this.texture);
+        drawTexturedModalRect(this.guiLeft, this.guiTop, 0, 0, this.xSize, this.ySize);
     }
 
     //opening the gui doesn't pause the game
