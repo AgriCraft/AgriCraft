574339332cb22402bfef238605671f2f67f1ed9d
Simon Zuend <simon.zuend@gmx.net>
2015-02-10 15:02:35 +0100

    Adds rake variants; english localization included.

--- src/main/java/com/InfinityRaider/AgriCraft/items/ItemHandRake.java
+++ src/main/java/com/InfinityRaider/AgriCraft/items/ItemHandRake.java
@@ -1,7 +1,11 @@
 package com.InfinityRaider.AgriCraft.items;
 
 import com.InfinityRaider.AgriCraft.creativetab.AgriCraftTab;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+import java.util.List;
 
 /**
  * Tool to uproot weeds.
@@ -9,8 +13,30 @@ import net.minecraft.item.Item;
  */
 public class ItemHandRake extends Item {
 
+    private static final int WOOD_VARIANT_META = 0;
+    private static final int IRON_VARIANT_META = 1;
+
     public ItemHandRake() {
         setCreativeTab(AgriCraftTab.agriCraftTab);
         maxStackSize = 1;
+        hasSubtypes = true;
+    }
+
+    @Override
+    public void getSubItems(Item item, CreativeTabs creativeTabs, List list) {
+        list.add(new ItemStack(item, 1, WOOD_VARIANT_META));
+        list.add(new ItemStack(item, 1, IRON_VARIANT_META));
+    }
+
+    @Override
+    public String getUnlocalizedName(ItemStack itemStack) {
+        String base = super.getUnlocalizedName(itemStack);
+        if (itemStack.getItemDamage() == WOOD_VARIANT_META) {
+            return base + ".wood";
+        } else if (itemStack.getItemDamage() == IRON_VARIANT_META) {
+            return base + ".iron";
+        } else {
+            throw new IllegalArgumentException("Unsupported meta value of " + itemStack.getItemDamage() + " for ItemHandRake.");
+        }
     }
 }
