030ffc04caa7091f068fdb01440c82a65048e4a4
InfinityRaider <InfinityRaider@users.noreply.github.com>
2015-03-05 14:51:19 +0100

    Started Thaumic Tinkerer compatibility

---
+++ src/main/java/com/InfinityRaider/AgriCraft/compatibility/thaumictinkerer/ThaumicTinkererOverride.java
@@ -0,0 +1,81 @@
+package com.InfinityRaider.AgriCraft.compatibility.thaumictinkerer;
+/*
+import com.InfinityRaider.AgriCraft.farming.CropOverride;
+import com.InfinityRaider.AgriCraft.tileentity.TileEntityCrop;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.IIcon;
+
+public class ThaumicTinkererOverride extends CropOverride {
+    private TileEntityCrop crop;
+
+    public ThaumicTinkererOverride(TileEntityCrop crop) {
+        this.crop = crop;
+    }
+
+    @Override
+    public void onSeedPlanted(EntityPlayer player) {
+
+    }
+
+    @Override
+    public boolean hasDefaultGrowth() {
+        return true;
+    }
+
+    @Override
+    public void increaseGrowth() {
+
+    }
+
+    @Override
+    public boolean hasDefaultBonemeal() {
+        return false;
+    }
+
+    @Override
+    public void applyBonemeal() {
+
+    }
+
+    @Override
+    public boolean hasDefaultHarvesting() {
+        return false;
+    }
+
+    @Override
+    public void onHarvest() {
+
+    }
+
+    @Override
+    public boolean hasDefaultBreaking() {
+        return false;
+    }
+
+    @Override
+    public void onBreak() {
+
+    }
+
+    @Override
+    public boolean immuneToWeed() {
+        return false;
+    }
+
+    @Override
+    public IIcon getIcon() {
+        return null;
+    }
+
+    @Override
+    public NBTTagCompound saveToNBT() {
+        return null;
+    }
+
+    @Override
+    public void loadFromNBT(NBTTagCompound tag) {
+
+    }
+}
+*/
