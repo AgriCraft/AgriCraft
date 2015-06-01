8614625a0bd655e223b5526bda66fca03f3da278
InfinityRaider <InfinityRaider@users.noreply.github.com>
2015-06-01 12:10:52 +0200

    javadoc

--- src/main/java/com/InfinityRaider/AgriCraft/apiimpl/v1/cropplant/CropPlant.java
+++ src/main/java/com/InfinityRaider/AgriCraft/apiimpl/v1/cropplant/CropPlant.java
@@ -29,6 +29,7 @@ public abstract class CropPlant {
         }
     }
 
+    /** This is called to get the actual tier of a seed */
     public final int getTier() {
         int seedTierOverride = SeedHelper.getSeedTierOverride(getSeed());
         if(seedTierOverride>0) {
@@ -37,46 +38,63 @@ public abstract class CropPlant {
         return tier();
     }
 
+    /** Gets the tier of this plant, can be overridden trough the configs */
     public abstract int tier();
 
+    /** Gets a stack of the seed for this plant */
     public abstract ItemStack getSeed();
 
+    /** Gets an arraylist of all possible fruit drops from this plant */
     public abstract ArrayList<ItemStack> getAllFruits();
 
+    /** Returns a random fruit for this plant */
     public abstract ItemStack getRandomFruit(Random rand);
 
+    /** Returns an ArrayList with amount of random fruit stacks for this plant */
     public abstract ArrayList<ItemStack> getFruitsOnHarvest(int gain, Random rand);
 
+    /** Gets called right before a harvest attempt, return false to prevent further processing, player may be null if harvested by automation */
     public boolean onHarvest(World world, int x, int y, int z, EntityPlayer player) {
         return true;
     }
 
+    /** This is called right after this plant is planted on a crop, either trough planting, mutation or spreading */
     public void onSeedPlanted(World world, int x, int y, int z) {}
 
+    /** This is called right after this plant is removed from a crop or a crop holding this plant is broken */
     public void onPlantRemoved(World world, int x, int y, int z) {}
 
+    /** Allow this plant to be bonemealed or not */
     public abstract boolean canBonemeal();
 
+    /** When a growth thick is allowed for this plant, return true to re-render the crop clientside */
     public abstract boolean onAllowedGrowthTick(World world, int x, int y, int z, int oldGrowthStage);
 
+    /** Checks if the plant can grow on this position */
     public abstract boolean isFertile(World world, int x, int y, int z);
 
+    /** Checks if the plant is mature */
     public boolean isMature(IBlockAccess world, int x, int y, int z) {
         return world.getBlockMetadata(x, y, z)>=7;
     }
 
+    /** Gets the height of the crop */
     @SideOnly(Side.CLIENT)
     public abstract float getHeight(int meta);
 
+    /** Gets the icon for the plant, growth stage goes from 0 to 7 (both inclusive, 0 is sprout and 7 is mature) */
     @SideOnly(Side.CLIENT)
     public abstract IIcon getPlantIcon(int growthStage);
 
+    /** Determines how the plant is rendered, return false to render as wheat (#), true to render as a flower (X) */
     @SideOnly(Side.CLIENT)
     public abstract boolean renderAsFlower();
 
+    /** Gets some information about the plant for the journal */
     @SideOnly(Side.CLIENT)
     public abstract String getInformation();
 
+    /** This is called when the plant is rendered */
     @SideOnly(Side.CLIENT)
     public void renderPlantInCrop(IBlockAccess world, int x, int y, int z, RenderBlocks renderer) {
         PlantRenderer.renderPlantLayer(x, y, z, renderer, renderAsFlower() ? 1 : 6, getPlantIcon(world.getBlockMetadata(x, y, z)), 0);
