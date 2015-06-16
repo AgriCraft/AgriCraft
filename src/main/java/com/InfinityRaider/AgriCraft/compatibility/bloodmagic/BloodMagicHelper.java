7790be56d0634515a67fa9b11c05a84384b3b17d
InfinityRaider <InfinityRaider@users.noreply.github.com>
2015-06-16 21:45:17 +0200

    Use reflection to avoid direct referencing to bloodmagic API (#226)

--- src/main/java/com/InfinityRaider/AgriCraft/compatibility/bloodmagic/BloodMagicHelper.java
+++ src/main/java/com/InfinityRaider/AgriCraft/compatibility/bloodmagic/BloodMagicHelper.java
@@ -1,13 +1,24 @@
 package com.InfinityRaider.AgriCraft.compatibility.bloodmagic;
 
-import WayofTime.alchemicalWizardry.api.harvest.HarvestRegistry;
 import com.InfinityRaider.AgriCraft.compatibility.ModHelper;
+import com.InfinityRaider.AgriCraft.handler.ConfigurationHandler;
 import com.InfinityRaider.AgriCraft.reference.Names;
+
+import java.lang.reflect.Method;
 
 public class BloodMagicHelper extends ModHelper {
     @Override
     protected void init() {
-        HarvestRegistry.registerHarvestHandler(new BloodMagicHarvesthandler());
+        try {
+            Class harvestRegistry = Class.forName("WayofTime.alchemicalWizardry.api.harvest.HarvestRegistry");
+            Class harvestHandler = Class.forName("WayofTime.alchemicalWizardry.api.harvest.IHarvestHandler");
+            Method registerHarvestHandler = harvestRegistry.getMethod("registerHarvestHandler", harvestHandler);
+            registerHarvestHandler.invoke(null, new BloodMagicHarvestHandler());
+        } catch(Exception e) {
+            if(ConfigurationHandler.debug) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
