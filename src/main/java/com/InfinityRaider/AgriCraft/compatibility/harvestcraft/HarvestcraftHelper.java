d39198f9ba07e4dab22a51827ad8e71f62e56a74
InfinityRaider <InfinityRaider@users.noreply.github.com>
2015-07-15 18:38:35 +0200

    Make harvestcraft rice and seewead require water pads as soil

--- src/main/java/com/InfinityRaider/AgriCraft/compatibility/harvestcraft/HarvestcraftHelper.java
+++ src/main/java/com/InfinityRaider/AgriCraft/compatibility/harvestcraft/HarvestcraftHelper.java
@@ -1,8 +1,11 @@
 package com.InfinityRaider.AgriCraft.compatibility.harvestcraft;
 
+import com.InfinityRaider.AgriCraft.api.v1.BlockWithMeta;
 import com.InfinityRaider.AgriCraft.compatibility.ModHelper;
 import com.InfinityRaider.AgriCraft.farming.CropPlantHandler;
+import com.InfinityRaider.AgriCraft.farming.GrowthRequirementHandler;
 import com.InfinityRaider.AgriCraft.handler.ConfigurationHandler;
+import com.InfinityRaider.AgriCraft.init.Blocks;
 import com.InfinityRaider.AgriCraft.reference.Names;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemSeeds;
@@ -35,11 +38,14 @@ public final class HarvestcraftHelper extends ModHelper {
                     Object obj = field.get(null);
                     if(obj instanceof ItemSeeds) {
                         ItemSeeds seed = (ItemSeeds) obj;
-                        if(seed== Item.itemRegistry.getObject("harvestcraft:cottonItem")) {
+                        if(seed == Item.itemRegistry.getObject("harvestcraft:cottonItem")) {
                             //cotton, the produce of cotton seeds is also instance of ItemSeeds, but we want to ignore it
                             continue;
                         }
                         CropPlantHandler.registerPlant(new CropPlantHarvestCraft(seed));
+                        if(seed == Item.itemRegistry.getObject("harvestcraft:seaweedseedItem") || seed == Item.itemRegistry.getObject("harvestcraft:riceseedItem")) {
+                            GrowthRequirementHandler.getGrowthRequirement(seed, 0).setSoil(new BlockWithMeta(Blocks.blockWaterPadFull));
+                        }
                     }
                 } catch (Exception e) {
                     if (ConfigurationHandler.debug) {
