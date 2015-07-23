19740cc5eee7a770611085357b533a21415c23f9
InfinityRaider <InfinityRaider@users.noreply.github.com>
2015-07-23 22:40:21 +0200

    Prepare compatibilty for LordOfTheRingsMod and FossilsAndArcheology plants

---
+++ src/main/java/com/InfinityRaider/AgriCraft/compatibility/fossilsandarcheology/FossilsAndArcheologyHelper.java
@@ -0,0 +1,20 @@
+package com.InfinityRaider.AgriCraft.compatibility.fossilsandarcheology;
+
+import com.InfinityRaider.AgriCraft.compatibility.ModHelper;
+
+public class FossilsAndArcheologyHelper extends ModHelper {
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected void initPlants() {
+
+    }
+
+    @Override
+    protected String modId() {
+        return null;
+    }
+}
---
+++ src/main/java/com/InfinityRaider/AgriCraft/compatibility/lordoftherings/LordOfTheRingsHelper.java
@@ -0,0 +1,20 @@
+package com.InfinityRaider.AgriCraft.compatibility.lordoftherings;
+
+import com.InfinityRaider.AgriCraft.compatibility.ModHelper;
+
+public class LordOfTheRingsHelper extends ModHelper {
+    @Override
+    protected void init() {
+        
+    }
+
+    @Override
+    protected void initPlants() {
+
+    }
+
+    @Override
+    protected String modId() {
+        return null;
+    }
+}
