6d12d9c5672896f851e2a65ba97e99c6e7bafa28
InfinityRaider <InfinityRaider@users.noreply.github.com>
2015-08-01 17:27:14 +0200

    Redirect methods to Tessellator.instance

--- src/main/java/com/InfinityRaider/AgriCraft/renderers/TessellatorV2.java
+++ src/main/java/com/InfinityRaider/AgriCraft/renderers/TessellatorV2.java
@@ -4,10 +4,12 @@ import com.InfinityRaider.AgriCraft.utility.TransformationMatrix;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.shader.TesselatorVertexState;
 
 @SideOnly(Side.CLIENT)
 public class TessellatorV2 extends Tessellator {
     public static final TessellatorV2 instance = new TessellatorV2(2097152);
+    private static final Tessellator tessellator = Tessellator.instance;
 
     private TransformationMatrix matrix = new TransformationMatrix();
 
@@ -16,6 +18,10 @@ public class TessellatorV2 extends Tessellator {
     static {
         instance.defaultTexture = true;
     }
+
+    //---------------------------------------------
+    //methods requiring some linear transformations
+    //---------------------------------------------
 
     /**
      * Adds a vertex specifying both x,y,z and the texture u,v for it.
@@ -33,7 +39,7 @@ public class TessellatorV2 extends Tessellator {
     @Override
     public void addVertex(double x, double y, double z) {
         double[] coords = this.matrix.transform(x, y, z);
-        super.addVertex(coords[0], coords[1], coords[2]);
+        tessellator.addVertex(coords[0], coords[1], coords[2]);
     }
 
     /**
@@ -62,6 +68,106 @@ public class TessellatorV2 extends Tessellator {
      */
     public void addRotation(double angle, double x, double y, double z) {
         this.matrix.multiplyLeftWith(new TransformationMatrix(angle, x, y, z));
+    }
+
+    //---------------
+    //other overrides
+    //---------------
+    public int draw() {
+        return tessellator.draw();
+    }
+
+    public TesselatorVertexState getVertexState(float x, float y, float z) {
+        return tessellator.getVertexState(x, y, z);
+    }
+
+    public void setVertexState(TesselatorVertexState state) {
+        tessellator.setVertexState(state);
+    }
+
+    /**
+     * Sets draw mode in the tessellator to draw quads.
+     */
+    public void startDrawingQuads() {
+        tessellator.startDrawing(7);
+    }
+
+    /**
+     * Resets tessellator state and prepares for drawing (with the specified draw mode).
+     */
+    public void startDrawing(int drawMode) {
+        tessellator.startDrawing(drawMode);
+    }
+
+    /**
+     * Sets the texture coordinates.
+     */
+    public void setTextureUV(double u, double v) {
+        tessellator.setTextureUV(u, v);
+    }
+
+    public void setBrightness(int value) {
+        tessellator.setBrightness(value);
+    }
+
+    /**
+     * Sets the RGB values as specified, converting from floats between 0 and 1 to integers from 0-255.
+     */
+    public void setColorOpaque_F(float red, float green, float blue) {
+        tessellator.setColorOpaque_F(red, green, blue);
+    }
+
+    /**
+     * Sets the RGBA values for the color, converting from floats between 0 and 1 to integers from 0-255.
+     */
+    public void setColorRGBA_F(float red, float green, float blue, float alpha) {
+        tessellator.setColorRGBA_F(red, green, blue, alpha);
+    }
+
+    /**
+     * Sets the RGB values as specified, and sets alpha to opaque.
+     */
+    public void setColorOpaque(int red, int green, int blue) {
+        tessellator.setColorOpaque(red, green, blue);
+    }
+
+    /**
+     * Sets the RGBA values for the color. Also clamps them to 0-255.
+     */
+    public void setColorRGBA(int red, int green, int blue, int alpha) {
+        tessellator.setColorRGBA(red, green, blue, alpha);
+    }
+
+    public void func_154352_a(byte red, byte green, byte blue) {
+        tessellator.func_154352_a(red, green, blue);
+    }
+
+    /**
+     * Sets the color to the given opaque value (stored as byte values packed in an integer).
+     */
+    public void setColorOpaque_I(int value) {
+        tessellator.setColorOpaque_I(value);
+    }
+
+    /**
+     * Sets the color to the given color (packed as bytes in integer) and alpha values.
+     */
+    public void setColorRGBA_I(int value, int alpha) {
+        tessellator.setColorRGBA_I(value, alpha);
+    }
+
+    /**
+     * Disables colors for the current draw call.
+     */
+    public void disableColor() {
+        tessellator.disableColor();
+    }
+
+    /**
+     * Sets the normal for the current draw call.
+     */
+    public void setNormal(float x, float y, float z) {
+        tessellator.setNormal(x, y, z);
     }
 
 }
