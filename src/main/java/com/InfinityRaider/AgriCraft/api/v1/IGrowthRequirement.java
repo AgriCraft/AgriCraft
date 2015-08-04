04d8848a6f50d151b1c629c131bb7a6295c63de0
InfinityRaider <InfinityRaider@users.noreply.github.com>
2015-08-04 21:27:02 +0200

    Clean up unnecessary 'public's

--- src/main/java/com/InfinityRaider/AgriCraft/api/v1/IGrowthRequirement.java
+++ src/main/java/com/InfinityRaider/AgriCraft/api/v1/IGrowthRequirement.java
@@ -6,41 +6,38 @@ public interface IGrowthRequirement {
 	//Methods to check if a seed can grow
 	//-----------------------------------
 	/** @return true, if all the requirements are met */
-	public boolean canGrow(World world, int x, int y, int z);
+	boolean canGrow(World world, int x, int y, int z);
 
 	//public boolean canPlant(World world, int x, int y, int z);
 
 	/** @return true, if the correct base block is present **/
-	public boolean isBaseBlockPresent(World world, int x, int y, int z);
-
-	/** @return true, if the light level is between the allowed values */
-	//public boolean isBrightnessGood(World world, int x, int y, int z);
+	boolean isBaseBlockPresent(World world, int x, int y, int z);
 
 	/** @return true, if the given block is a valid soil */
-	public boolean isValidSoil(World world, int x, int y, int z);
+	boolean isValidSoil(World world, int x, int y, int z);
 
 	/** @return true, if the given block is a valid soil */
-	public boolean isValidSoil(BlockWithMeta soil);
+	boolean isValidSoil(BlockWithMeta soil);
 
 	/** @return the required block as ItemStack of size 1 */
-	public ItemStack requiredBlockAsItemStack();
+	ItemStack requiredBlockAsItemStack();
 
-	public RequirementType getRequiredType();
+	RequirementType getRequiredType();
 
 	//Methods to change specific requirements
 	//--------------------------------------
-	public BlockWithMeta getSoil();
+	BlockWithMeta getSoil();
 
-	public void setSoil(BlockWithMeta soil);
+	void setSoil(BlockWithMeta soil);
 
-	public int[] getBrightnessRange();
+	int[] getBrightnessRange();
 
-	public void setBrightnessRange(int min, int max);
+	void setBrightnessRange(int min, int max);
 
-	public void setRequiredBlock(BlockWithMeta requiredBlock, RequirementType requirementType, boolean oreDict);
+	void setRequiredBlock(BlockWithMeta requiredBlock, RequirementType requirementType, boolean oreDict);
 
-	public BlockWithMeta getRequiredBlock();
+	BlockWithMeta getRequiredBlock();
 
-	public boolean isOreDict();
+	boolean isOreDict();
 
 }
