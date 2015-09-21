741a18a7c72aa7510551b2627ff10bbfba24b0a9
RlonRyan <rlonryan@aol.com>
2015-09-21 12:57:44 -0600

    Separate MultiBlocks from Tanks
    Hopefully this will make the tanks more manageable.

--- src/main/java/com/InfinityRaider/AgriCraft/tileentity/irrigation/IIrrigationComponent.java
+++ src/main/java/com/InfinityRaider/AgriCraft/tileentity/irrigation/IIrrigationComponent.java
@@ -3,11 +3,16 @@ package com.InfinityRaider.AgriCraft.tileentity.irrigation;
 
 /**
  * The base for all irrigation components in AgriCraft.
+ * <p>
+ * <i>Java 8 would make this so much easier, with default methods.</i>
+ * <p>
+ * @since 1.3
  */
 public interface IIrrigationComponent {
 
 	/**
 	 * Sets the irrigation component's contained fluid level.
+	 * Don't use this method to transfer fluids between containers.
 	 * 
 	 * @param lvl the level to set the fluid to.
 	 */
@@ -26,6 +31,15 @@ public interface IIrrigationComponent {
 	 * @return the component's maximum fluid level.
 	 */
 	int getCapacity();
+	
+	/**
+	 * Determines the water height, relative to the bottom of the block, and scaled on the 0 - {@value Constants.WHOLE} interval.
+	 * <p>
+	 * <b>Notice</b>: This method is not to be considered efficient, due to the rounding required.
+	 * </p>
+	 * @return the component's fluid height.
+	 */
+	float getFluidHeight();
 
 	/**
 	 * Determines if a component may connect to another component.
@@ -34,5 +48,21 @@ public interface IIrrigationComponent {
 	 * @return if the component may connect to the given component.
 	 */
 	boolean canConnectTo(IIrrigationComponent component);
+	
+	/**
+	 * Attempts to add fluid to an irrigation component.
+	 * 
+	 * @param amount the amount to add.
+	 * @return the amount of fluid left over.
+	 */
+	//int pushFluid(int amount);
+	
+	/**
+	 * Attempts to remove an amount of fluid from the irrigation component.
+	 * 
+	 * @param amount the amount of fluid desired to be removed.
+	 * @return the amount of fluid <em>actually</em> removed.
+	 */
+	//int pullFluid(int amount);
 
 }
