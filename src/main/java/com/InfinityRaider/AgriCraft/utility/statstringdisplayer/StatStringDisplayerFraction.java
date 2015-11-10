5ea974dbd9665885edd3ce1205b59258e6fef1e8
InfinityRaider <InfinityRaider@users.noreply.github.com>
2015-11-10 18:01:16 +0100

    Missing @SideOnly(Side.CLIENT) annotation

--- src/main/java/com/InfinityRaider/AgriCraft/utility/statstringdisplayer/StatStringDisplayerFraction.java
+++ src/main/java/com/InfinityRaider/AgriCraft/utility/statstringdisplayer/StatStringDisplayerFraction.java
@@ -1,5 +1,9 @@
 package com.InfinityRaider.AgriCraft.utility.statstringdisplayer;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
 public class StatStringDisplayerFraction extends StatStringDisplayer {
     @Override
     public String getStatDisplayString(int stat, int max) {
