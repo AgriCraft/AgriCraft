3ef1a6ea723e22340d553bafebcc975f3195f0f9
InfinityRaider <InfinityRaider@users.noreply.github.com>
2015-11-24 01:36:56 +0100

    Actually fix build error -.- (hopefully)

--- src/api/java/tehnut/resourceful/crops/api/util/helper/ItemHelper.java
+++ src/api/java/tehnut/resourceful/crops/api/util/helper/ItemHelper.java
@@ -5,7 +5,6 @@ import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
-import tehnut.resourceful.crops.util.helper.LogHelper;
 
 public class ItemHelper {
 
