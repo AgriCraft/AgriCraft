dd6422d91d75ba188c8f325ffe327fb68fddfd20
InfinityRaider <InfinityRaider@users.noreply.github.com>
2015-12-12 16:52:04 +0100

    Create cross crops while sneaking

--- src/main/java/com/InfinityRaider/AgriCraft/items/ItemCrop.java
+++ src/main/java/com/InfinityRaider/AgriCraft/items/ItemCrop.java
@@ -4,11 +4,13 @@ import com.InfinityRaider.AgriCraft.farming.growthrequirement.GrowthRequirementH
 import com.InfinityRaider.AgriCraft.init.Blocks;
 import com.InfinityRaider.AgriCraft.reference.Names;
 import com.InfinityRaider.AgriCraft.renderers.items.RenderItemBase;
+import com.InfinityRaider.AgriCraft.tileentity.TileEntityCrop;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
 public class ItemCrop extends ItemAgricraft {
@@ -33,8 +35,16 @@ public class ItemCrop extends ItemAgricraft {
         if (!world.isRemote) {
             if (GrowthRequirementHandler.isSoilValid(world, x, y, z) && world.getBlock(x, y + 1, z).getMaterial()== Material.air && side == 1) {
                 world.setBlock(x, y + 1, z, Blocks.blockCrop);
+                int use = 1;
+                if(player.isSneaking() && (player.capabilities.isCreativeMode || stack.stackSize>=2)) {
+                    TileEntity tile = world.getTileEntity(x, y+1, z);
+                    if(tile != null && (tile instanceof TileEntityCrop)) {
+                        ((TileEntityCrop) tile).setCrossCrop(true);
+                        use = 2;
+                    }
+                }
                 world.playSoundEffect((double)((float) x + 0.5F), (double)((float) y + 0.5F), (double)((float) z + 0.5F), net.minecraft.init.Blocks.leaves.stepSound.func_150496_b(), (net.minecraft.init.Blocks.leaves.stepSound.getVolume() + 1.0F) / 2.0F, net.minecraft.init.Blocks.leaves.stepSound.getPitch() * 0.8F);
-                stack.stackSize = player.capabilities.isCreativeMode ? stack.stackSize : stack.stackSize - 1;
+                stack.stackSize = player.capabilities.isCreativeMode ? stack.stackSize : stack.stackSize - use;
                 return false;
             }
         }
