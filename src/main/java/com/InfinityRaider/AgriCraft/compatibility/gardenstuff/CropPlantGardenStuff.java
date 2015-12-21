5a49d0bd39cf2b806dcacde93ccc096de1fb14c4
InfinityRaider <InfinityRaider@users.noreply.github.com>
2015-12-21 15:14:02 +0100

    Add support for Garden Stuff candelilla seeds (#515)

---
+++ src/main/java/com/InfinityRaider/AgriCraft/compatibility/gardenstuff/CropPlantGardenStuff.java
@@ -0,0 +1,102 @@
+package com.InfinityRaider.AgriCraft.compatibility.gardenstuff;
+
+import com.InfinityRaider.AgriCraft.api.v1.IGrowthRequirement;
+import com.InfinityRaider.AgriCraft.farming.cropplant.CropPlant;
+import com.InfinityRaider.AgriCraft.farming.growthrequirement.GrowthRequirementHandler;
+import com.InfinityRaider.AgriCraft.reference.Constants;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
+import net.minecraft.world.World;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class CropPlantGardenStuff extends CropPlant {
+    private final Item seed;
+    private final Block plant;
+    private final Item fruit;
+
+    protected CropPlantGardenStuff(Item seed, Block plant, Item fruit) {
+        super();
+        this.seed = seed;
+        this.plant = plant;
+        this.fruit = fruit;
+    }
+
+    @Override
+    public int tier() {
+        return 2;
+    }
+
+    @Override
+    public ItemStack getSeed() {
+        return new ItemStack(seed);
+    }
+
+    @Override
+    public Block getBlock() {
+        return plant;
+    }
+
+    @Override
+    public ArrayList<ItemStack> getAllFruits() {
+        ArrayList<ItemStack> fruits = new ArrayList<ItemStack>();
+        fruits.add(new ItemStack(fruit));
+        return fruits;
+    }
+
+    @Override
+    public ItemStack getRandomFruit(Random rand) {
+        return new ItemStack(fruit);
+    }
+
+    @Override
+    public ArrayList<ItemStack> getFruitsOnHarvest(int gain, Random rand) {
+        int amount = (int) (Math.ceil((gain + 0.00) / 3));
+        ArrayList<ItemStack> list = new ArrayList<ItemStack>();
+        while(amount>0) {
+            list.add(getRandomFruit(rand));
+            amount--;
+        }
+        return list;
+    }
+
+    @Override
+    public boolean canBonemeal() {
+        return this.getTier() <= 3;
+    }
+
+    @Override
+    protected IGrowthRequirement initGrowthRequirement() {
+        return GrowthRequirementHandler.getNewBuilder().build();
+    }
+
+    @Override
+    public boolean onAllowedGrowthTick(World world, int x, int y, int z, int oldGrowthStage) {
+        return true;
+    }
+
+    @Override
+    public float getHeight(int meta) {
+        return Constants.UNIT*13;
+    }
+
+    @Override
+    public IIcon getPlantIcon(int growthStage) {
+        return plant.getIcon(0, growthStage);
+    }
+
+    @Override
+    public boolean renderAsFlower() {
+        return false;
+    }
+
+    @Override
+    public String getInformation() {
+        String name = seed.getUnlocalizedName();
+        String[] split = name.split("\\.");
+        return "agricraft_journal.gardenstuff_"+(split.length == 0 ? name : split[split.length-1]);
+    }
+}
--- src/main/java/com/InfinityRaider/AgriCraft/compatibility/gardenstuff/GardenStuffHelper.java
+++ src/main/java/com/InfinityRaider/AgriCraft/compatibility/gardenstuff/GardenStuffHelper.java
@@ -2,11 +2,30 @@ package com.InfinityRaider.AgriCraft.compatibility.gardenstuff;
 
 import com.InfinityRaider.AgriCraft.api.v1.BlockWithMeta;
 import com.InfinityRaider.AgriCraft.compatibility.ModHelper;
+import com.InfinityRaider.AgriCraft.farming.CropPlantHandler;
 import com.InfinityRaider.AgriCraft.farming.growthrequirement.GrowthRequirementHandler;
 import com.InfinityRaider.AgriCraft.reference.Names;
+import com.InfinityRaider.AgriCraft.utility.LogHelper;
+import com.InfinityRaider.AgriCraft.utility.exception.DuplicateCropPlantException;
+
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 
 public class GardenStuffHelper extends ModHelper {
+    @Override
+    protected void initPlants() {
+        Item seed = (Item) Item.itemRegistry.getObject("GardenTrees:candelilla_seeds");
+        Item fruit = (Item) Item.itemRegistry.getObject("GardenTrees:candelilla");
+        Block plant = (Block) Block.blockRegistry.getObject("GardenTrees:candelilla_bush");
+        if(seed != null && fruit != null && plant != null) {
+            try {
+                CropPlantHandler.registerPlant(new CropPlantGardenStuff(seed, plant, fruit));
+            } catch (DuplicateCropPlantException e) {
+                LogHelper.printStackTrace(e);
+            }
+        }
+    }
+
     @Override
     protected void onPostInit() {
         registerSoils();
