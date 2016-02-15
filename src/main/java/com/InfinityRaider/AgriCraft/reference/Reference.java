c201a72b6f8fe1e8c10e9b1c3231b340b5566639
RlonRyan <rlonryan@aol.com>
2016-02-15 11:55:38 -0700

    Convert To QuikMod Buildscript
    For easier version tracking and parameter replacement. Furthermore, you
    can set the run location with a private gradle.properties file now.

--- src/main/java/com/InfinityRaider/AgriCraft/reference/Reference.java
+++ src/main/java/com/InfinityRaider/AgriCraft/reference/Reference.java
@@ -7,17 +7,17 @@ package com.InfinityRaider.AgriCraft.reference;
  */
 @Deprecated
 public final class Reference {
-    public static final String MOD_NAME = "AgriCraft";
-    public static final String MOD_ID = "AgriCraft";
+    public static final String MOD_NAME = "${mod.name}";
+    public static final String MOD_ID = "${mod.id}";
 
-    public static final String VER_MAJOR = "@MAJOR@";
-    public static final String VER_MINOR = "@MINOR@";
-    public static final String VER_REVIS = "@REVIS@";
-    public static final String MOD_VERSION = VER_MAJOR + "." + VER_MINOR + "." + VER_REVIS;
-    public static final String VERSION = "1.8.8-" + MOD_VERSION;
+    public static final String VER_MAJOR = "${mod.version_major}";
+    public static final String VER_MINOR = "${mod.version_minor}";
+    public static final String VER_REVIS = "${mod.version_patch}";
+    public static final String MOD_VERSION = "${mod.version}";
+    public static final String VERSION = "${mod.version_minecraft}-${mod.version}";
 
-    public static final String AUTHOR = "InfinityRaider";
-    public static final String CLIENT_PROXY_CLASS = "com.InfinityRaider.AgriCraft.proxy.ClientProxy";
-    public static final String SERVER_PROXY_CLASS = "com.InfinityRaider.AgriCraft.proxy.ServerProxy";
-    public static final String GUI_FACTORY_CLASS = "com.InfinityRaider.AgriCraft.gui.GuiFactory";
+    public static final String AUTHOR = "${mod.author}";
+    public static final String CLIENT_PROXY_CLASS = "${mod.group}.${mod.author}.${mod.id}.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "${mod.group}.${mod.author}.${mod.id}.proxy.ServerProxy";
+    public static final String GUI_FACTORY_CLASS = "${mod.group}.${mod.author}.${mod.id}.gui.GuiFactory";
 }
