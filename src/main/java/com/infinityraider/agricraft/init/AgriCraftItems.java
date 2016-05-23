9f0782a39fe547da34f4ee20e6c378a5a77c3257
RlonRyan <rlonryan@aol.com>
2016-05-23 15:30:15 -0600

    Transfer Configuration Handling
    The core module now handles configuration for the mod.

--- src/main/java/com/infinityraider/agricraft/init/AgriCraftItems.java
+++ src/main/java/com/infinityraider/agricraft/init/AgriCraftItems.java
@@ -1,44 +1,43 @@
 package com.infinityraider.agricraft.init;
 
-import com.infinityraider.agricraft.handler.config.AgriCraftConfigurable;
-import com.infinityraider.agricraft.handler.config.ConfigCategory;
-import com.infinityraider.agricraft.handler.config.ConfigurationHandler;
+import com.agricraft.agricore.config.AgriConfigCategory;
+import com.agricraft.agricore.config.AgriConfigurable;
 import com.infinityraider.agricraft.items.*;
 import com.agricraft.agricore.core.AgriCore;
 import net.minecraft.item.Item;
 
 public class AgriCraftItems {
 
-	@AgriCraftConfigurable(
-			category = ConfigCategory.TOOLS,
+	@AgriConfigurable(
+			category = AgriConfigCategory.TOOLS,
 			key = "Enable Hand Rake",
 			comment = "Set to false to disable the Hand Rake."
 	)
 	public static boolean enableHandRake = true;
 
-	@AgriCraftConfigurable(
-			category = ConfigCategory.TOOLS,
+	@AgriConfigurable(
+			category = AgriConfigCategory.TOOLS,
 			key = "Enable Trowel",
 			comment = "Set to false to disable the Trowel."
 	)
 	public static boolean enableTrowel = true;
 
-	@AgriCraftConfigurable(
-			category = ConfigCategory.TOOLS,
+	@AgriConfigurable(
+			category = AgriConfigCategory.TOOLS,
 			key = "Enable Magnifying Glass",
 			comment = "Set to false to disable the Magnifying Glass."
 	)
 	public static boolean enableMagnifyingGlass = true;
 
-	@AgriCraftConfigurable(
-			category = ConfigCategory.TOOLS,
+	@AgriConfigurable(
+			category = AgriConfigCategory.TOOLS,
 			key = "Enable Clipper",
 			comment = "Set to false to disable the Clipper."
 	)
 	public static boolean enableClipper = true;
 	
 	static {
-		ConfigurationHandler.addConfigurable(AgriCraftItems.class);
+		AgriCore.getConfig().addConfigurable(AgriCraftItems.class);
 	}
 
 	public static Item crops;
