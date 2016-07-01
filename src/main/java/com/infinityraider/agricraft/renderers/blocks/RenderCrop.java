1803fd58cc383ec8b9fecd5e166abc4ea0f54e3a
RlonRyan <rlonryan@aol.com>
2016-07-01 15:06:08 -0600

    Modify Crop Rendering
    A first attempt at using a hybrid JSON approach.

--- src/main/java/com/infinityraider/agricraft/renderers/blocks/RenderCrop.java
+++ src/main/java/com/infinityraider/agricraft/renderers/blocks/RenderCrop.java
@@ -24,12 +24,10 @@ import javax.annotation.Nullable;
 @SideOnly(Side.CLIENT)
 public class RenderCrop extends RenderBlockBase<TileEntityCrop> {
 
-	private final ResourceLocation cropTexture;
 	private final ResourceLocation[] weedTextures;
 
 	public RenderCrop(BlockCrop block) {
 		super(block, new TileEntityCrop(), false, false, true);
-		this.cropTexture = block.getBlockTexture();
 		this.weedTextures = new ResourceLocation[16];
 		for (int i = 0; i < weedTextures.length; i++) {
 			weedTextures[i] = block.getWeedTexture(i);
@@ -40,23 +38,7 @@ public class RenderCrop extends RenderBlockBase<TileEntityCrop> {
 	public void renderWorldBlock(ITessellator tessellator, World world, BlockPos pos, double x, double y, double z, IBlockState state, Block block,
 			@Nullable TileEntityCrop crop, boolean dynamicRender, float partialTick, int destroyStage) {
 		if (crop != null) {
-			TextureAtlasSprite icon = tessellator.getIcon(cropTexture);
-			// Draw Vertical Bars
-			tessellator.pushMatrix();
-			tessellator.translate(0, -3 * Constants.UNIT, 0);
-			tessellator.drawScaledPrism(2, 0, 2, 3, 16, 3, icon);
-			tessellator.drawScaledPrism(13, 0, 2, 14, 16, 3, icon);
-			tessellator.drawScaledPrism(13, 0, 13, 14, 16, 14, icon);
-			tessellator.drawScaledPrism(2, 0, 13, 3, 16, 14, icon);
-			tessellator.popMatrix();
-
-			// Draw Horizontal Bars
-			if (crop.isCrossCrop()) {
-				tessellator.drawScaledPrism(0, 10, 2, 16, 11, 3, icon);
-				tessellator.drawScaledPrism(0, 10, 13, 16, 11, 14, icon);
-				tessellator.drawScaledPrism(2, 10, 0, 3, 11, 16, icon);
-				tessellator.drawScaledPrism(13, 10, 0, 14, 11, 16, icon);
-			} else if (crop.hasPlant()) {
+			if (crop.hasPlant()) {
 				//render the plant
 				PlantRenderer.renderPlant(world, BlockPos.ORIGIN, crop.getGrowthStage(), crop.getPlant(), tessellator);
 			} else if (crop.hasWeed()) {
