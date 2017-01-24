7ea0dabc11096a35342a11ce0e930a4cc96b93a0
RlonRyan <rlonryan@aol.com>
2017-01-23 23:41:01 -0700

    Clean Shapless Custom Wood Recipe
    Since it was quite the mess. Hopefully this won't wind up breaking it...

--- src/main/java/com/infinityraider/agricraft/crafting/RecipeShapelessCustomWood.java
+++ src/main/java/com/infinityraider/agricraft/crafting/RecipeShapelessCustomWood.java
@@ -1,6 +1,6 @@
 package com.infinityraider.agricraft.crafting;
 
-
+import com.agricraft.agricore.util.TypeHelper;
 import com.infinityraider.agricraft.items.blocks.ItemBlockCustomWood;
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
@@ -20,36 +20,44 @@ public class RecipeShapelessCustomWood extends ShapelessRecipes {
     @Override
     @SuppressWarnings("unchecked")
     public boolean matches(InventoryCrafting inventoryCrafting, World world) {
-        List recipeItems = new ArrayList(this.recipeItems);
-        for(int column = 0; column < 3; column++) {
-            for(int row = 0; row < 3; row++) {
-                ItemStack itemStackToMatch = inventoryCrafting.getStackInRowAndColumn(row, column);
-                if(itemStackToMatch != null) {
+        // Copy recipe
+        final List elements = new ArrayList(this.recipeItems);
+        for (int column = 0; column < 3; column++) {
+            for (int row = 0; row < 3; row++) {
+                final ItemStack toMatch = inventoryCrafting.getStackInRowAndColumn(row, column);
+                if (toMatch != null) {
                     boolean match = false;
-                    for (Object recipeItem : recipeItems) {
-                        ItemStack itemStack = (ItemStack) recipeItem;
-                        if (itemStackToMatch.getItem() == itemStack.getItem() && (itemStack.getItemDamage() == 32767 || itemStackToMatch.getItemDamage() == itemStack.getItemDamage())) {
-                            if (itemStackToMatch.getItem() instanceof ItemBlockCustomWood) {
-                                NBTTagCompound tag = itemStack.getTagCompound();
-                                NBTTagCompound tagToMatch = itemStackToMatch.getTagCompound();
-                                if (tagToMatch !=null && tag != null && tagToMatch.equals(tag)) {
-                                    match = true;
-                                    recipeItems.remove(itemStack);
-                                    break;
-                                }
-                            } else {
-                                match = true;
-                                recipeItems.remove(itemStack);
-                                break;
-                            }
+                    for (Object o : elements) {
+                        if (stacksMatch(toMatch, (ItemStack) o)) {
+                            elements.remove(o);
+                            match = true;
+                            break;
                         }
                     }
-                    if(!match) {
+                    if (!match) {
                         return false;
                     }
                 }
             }
         }
-        return recipeItems.isEmpty();
+        return elements.isEmpty();
     }
+
+    private boolean stacksMatch(ItemStack itemStackToMatch, ItemStack itemStack) {
+        if (itemStackToMatch.getItem() == itemStack.getItem() && metasMatch(itemStackToMatch, itemStack)) {
+            if (itemStackToMatch.getItem() instanceof ItemBlockCustomWood) {
+                NBTTagCompound tag = itemStack.getTagCompound();
+                NBTTagCompound tagToMatch = itemStackToMatch.getTagCompound();
+                return tagToMatch != null && tag != null && tagToMatch.equals(tag);
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean metasMatch(ItemStack itemStackToMatch, ItemStack itemStack) {
+        return itemStack.getItemDamage() == 32767 || itemStackToMatch.getItemDamage() == itemStack.getItemDamage();
+    }
+
 }
