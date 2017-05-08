916672292086524d0abfa8b75d62b662247765ff
RlonRyan <rlonryan@aol.com>
2017-05-07 18:33:29 -0600

    Implement OneProbe Integration
    OneProbe integration is nowhere near as clean as Waila integration...

---
+++ src/main/java/com/infinityraider/agricraft/compat/theoneprobe/TheOneProbePlugin.java
@@ -0,0 +1,22 @@
+package com.infinityraider.agricraft.compat.theoneprobe;
+
+import com.agricraft.agricore.core.AgriCore;
+import com.infinityraider.agricraft.api.AgriPlugin;
+import com.infinityraider.agricraft.api.IAgriPlugin;
+import net.minecraftforge.fml.common.event.FMLInterModComms;
+
+@AgriPlugin
+public class TheOneProbePlugin implements IAgriPlugin {
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public void initPlugin() {
+        AgriCore.getLogger("agricraft").debug("Calling One Probe Register! Result: {0}",
+        FMLInterModComms.sendFunctionMessage("theoneprobe", "getTheOneProbe", this.getClass().getPackage().getName() + ".GetTheOneProbe"));
+    }
+
+}
