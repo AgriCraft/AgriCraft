f465fba29a4218484cb25517df1e15bd8cc87682
RlonRyan <rlonryan@aol.com>
2017-05-14 11:46:46 -0600

    Add Sneak to Place Cross-Crops
    This commit re-adds the sneak to place cross-crops ability that was
    available in 1.7.10

--- src/main/java/com/infinityraider/agricraft/items/ItemCrop.java
+++ src/main/java/com/infinityraider/agricraft/items/ItemCrop.java
@@ -4,11 +4,10 @@ import com.infinityraider.agricraft.farming.growthrequirement.GrowthRequirementH
 import com.infinityraider.agricraft.init.AgriBlocks;
 import com.infinityraider.agricraft.items.tabs.AgriTabs;
 import com.infinityraider.agricraft.reference.AgriCraftConfig;
+import com.infinityraider.agricraft.tiles.TileEntityCrop;
 import com.infinityraider.infinitylib.item.IItemWithModel;
 import com.infinityraider.infinitylib.item.ItemBase;
 import net.minecraft.block.SoundType;
-import net.minecraft.block.material.Material;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
@@ -20,6 +19,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 import com.infinityraider.infinitylib.utility.IRecipeRegister;
+import com.infinityraider.infinitylib.utility.WorldHelper;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ItemCrop extends ItemBase implements IItemWithModel, IRecipeRegister {
@@ -35,22 +35,44 @@ public class ItemCrop extends ItemBase implements IItemWithModel, IRecipeRegiste
         return true;
     }
 
-    //this is called when you right click with this item in hand
+    // This is called when you right click with this item in hand.
     @Override
     public EnumActionResult onItemUseFirst(ItemStack stack, EntityPlayer player, World world, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ, EnumHand hand) {
-        if (!world.isRemote) {
-            BlockPos cropPos = pos.up();
-            IBlockState state = world.getBlockState(cropPos);
-            if (state.getBlock().getMaterial(state) == Material.AIR && GrowthRequirementHandler.isSoilValid(world, pos) && side == EnumFacing.UP) {
-                world.setBlockState(pos.up(), AgriBlocks.getInstance().CROP.getDefaultState());
-                int use = 1;
-                SoundType type = Blocks.LEAVES.getSoundType();
-                world.playSound(null, (double) ((float) cropPos.getX() + 0.5F), (double) ((float) cropPos.getY() + 0.5F), (double) ((float) cropPos.getZ() + 0.5F), type.getPlaceSound(), SoundCategory.PLAYERS, (type.getVolume() + 1.0F) / 4.0F, type.getPitch() * 0.8F);
-                stack.stackSize = player.capabilities.isCreativeMode ? stack.stackSize : stack.stackSize - use;
-                return EnumActionResult.SUCCESS;
-            }
+        // Skip if remote, or clicked wrong face of block.
+        if (world.isRemote || side != EnumFacing.UP) {
+            return EnumActionResult.PASS;
         }
-        return EnumActionResult.PASS;   //return false or else no other use methods will be called (for instance "onBlockActivated" on the crops block)
+
+        // Fetch information.
+        final BlockPos cropPos = pos.up();
+
+        // Test if placement is valid.
+        if (!world.isAirBlock(cropPos) || !GrowthRequirementHandler.isSoilValid(world, pos)) {
+            return EnumActionResult.FAIL;
+        }
+
+        // Set the block to a crop.
+        world.setBlockState(pos.up(), AgriBlocks.getInstance().CROP.getDefaultState());
+
+        // Remove the crop used from the stack.
+        stack.stackSize = player.capabilities.isCreativeMode ? stack.stackSize : stack.stackSize - 1;
+
+        // Handle sneak placing of crosscrops.
+        if (player.isSneaking() && stack.stackSize > 0) {
+            WorldHelper
+                    .getTile(world, pos.add(0, 1, 0), TileEntityCrop.class)
+                    .ifPresent(c -> {
+                        c.setCrossCrop(true);
+                        stack.stackSize = player.capabilities.isCreativeMode ? stack.stackSize : stack.stackSize - 1;
+                    });
+        }
+
+        // Play placement sound.
+        SoundType type = Blocks.LEAVES.getSoundType();
+        world.playSound(null, (double) ((float) cropPos.getX() + 0.5F), (double) ((float) cropPos.getY() + 0.5F), (double) ((float) cropPos.getZ() + 0.5F), type.getPlaceSound(), SoundCategory.PLAYERS, (type.getVolume() + 1.0F) / 4.0F, type.getPitch() * 0.8F);
+
+        // Action was a success.
+        return EnumActionResult.SUCCESS;
     }
 
     @Override
