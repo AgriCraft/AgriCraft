7dba66dab42bec250a33da93a2d7ff82a2cb0c26
CodesCubesAndCrashes <CodesCubesAndCrashes@users.noreply.github.com>
2017-06-10 22:48:57 -0400

    Replace texture after rendering sprinkler particle
    The original particle texture wasn't being bound again to the renderer,
    and so all particles would use the wrong texture while the sprinkler was
    active.
    
    This commit adds an overriding render method in LiquidSprayFX that sets
    the texture back after calling the original parent render method.

--- src/main/java/com/infinityraider/agricraft/renderers/particles/LiquidSprayFX.java
+++ src/main/java/com/infinityraider/agricraft/renderers/particles/LiquidSprayFX.java
@@ -2,6 +2,10 @@ package com.infinityraider.agricraft.renderers.particles;
 
 //heavily inspired by the OpenBlocks sprinkler
 import com.infinityraider.agricraft.utility.BaseIcons;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.VertexBuffer;
+import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
@@ -15,5 +19,12 @@ public class LiquidSprayFX extends AgriCraftFX {
         super(world, x, y, z, scale, gravity, vector, new ResourceLocation("minecraft:textures/blocks/water_still.png"));
         this.particleMaxAge = 15;
         this.setSize(0.2f, 0.2f);
+    }
+
+    @Override
+    public void renderParticle(VertexBuffer worldRenderer, Entity entity, float partialTicks, float f0, float f1, float f2, float f3, float f4) {
+        Minecraft.getMinecraft().renderEngine.bindTexture(this.texture);
+        super.renderParticle(worldRenderer, entity, partialTicks, f0, f1, f2, f3, f4);
+        Minecraft.getMinecraft().renderEngine.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
     }
 }
