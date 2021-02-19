1a8c14341dc444e6105010e0ac66a2a17b2388c6
InfinityRaider <theoneandonlyraider@gmail.com>
2021-02-19 17:53:50 +0000

    Add recipe for Grates

--- src/main/java/com/infinityraider/agricraft/content/decoration/BlockGrate.java
+++ src/main/java/com/infinityraider/agricraft/content/decoration/BlockGrate.java
@@ -10,6 +10,7 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
+import net.minecraft.util.IStringSerializable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
@@ -21,15 +22,13 @@ import java.util.function.BiFunction;
 public class BlockGrate extends BlockDynamicTexture<TileEntityGrate> {
     // Properties
     public static final InfProperty<Direction.Axis> AXIS = InfProperty.Creators.create("direction", Direction.Axis.X);
-    public static final InfProperty<Integer> OFFSET = InfProperty.Creators.create("offset", 0, -1, 1);
-    public static final InfProperty<Boolean> VINES_FRONT = InfProperty.Creators.create("vines_front", false);
-    public static final InfProperty<Boolean> VINES_BACK = InfProperty.Creators.create("vines_back", false);
+    public static final InfProperty<Offset> OFFSET = InfProperty.Creators.create("offset", Offset.class, Offset.MID);
+    public static final InfProperty<Vines> VINES = InfProperty.Creators.create("vines", Vines.class, Vines.NONE);
 
     private static final InfPropertyConfiguration PROPERTIES = InfPropertyConfiguration.builder()
             .add(AXIS)
             .add(OFFSET)
-            .add(VINES_FRONT)
-            .add(VINES_BACK)
+            .add(VINES)
             .waterloggable()
             .build();
 
@@ -52,5 +51,29 @@ public class BlockGrate extends BlockDynamicTexture<TileEntityGrate> {
 
     @Override
     public void onBlockPlacedBy(@Nonnull World world, @Nonnull BlockPos pos, @Nonnull BlockState state,
-                                @Nullable LivingEntity placer, @Nonnull ItemStack stack, @Nullable TileEntity tile) {}
+                                @Nullable LivingEntity placer, @Nonnull ItemStack stack, @Nullable TileEntity tile) {
+    }
+
+    public enum Offset implements IStringSerializable {
+        NEAR,
+        MID,
+        FAR;
+
+        @Override
+        public String getString() {
+            return this.name().toLowerCase();
+        }
+    }
+
+    public enum Vines implements IStringSerializable {
+        NONE,
+        FRONT,
+        BACK,
+        BOTH;
+
+        @Override
+        public String getString() {
+            return this.name().toLowerCase();
+        }
+    }
 }
