4575b166efed279170e6810d6bf4a83bca107878
InfinityRaider <theoneandonlyraider@gmail.com>
2021-04-12 17:01:45 +0100

    Fix JEI Focus bug

--- src/main/java/com/infinityraider/agricraft/plugins/jei/AgriClippingRecipeCategory.java
+++ src/main/java/com/infinityraider/agricraft/plugins/jei/AgriClippingRecipeCategory.java
@@ -88,6 +88,10 @@ public class AgriClippingRecipeCategory implements IRecipeCategory<IAgriPlant> {
 
     @Override
     public void setRecipe(IRecipeLayout layout, @Nonnull IAgriPlant plant, @Nonnull IIngredients ingredients) {
+        // Clear the focus as this sometimes causes display bugs
+        layout.getIngredientsGroup(AgriPlantIngredient.TYPE).setOverrideDisplayFocus(null);
+        layout.getIngredientsGroup(VanillaTypes.ITEM).setOverrideDisplayFocus(null);
+        
         // Denote that this is a shapeless recipe.
         layout.setShapeless();
 
