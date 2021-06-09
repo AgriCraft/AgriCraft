d1795671e5bc5fdafb405110d343a107ffdacb2d
InfinityRaider <theoneandonlyraider@gmail.com>
2021-06-09 09:30:32 +0100

    Add Simply Seasons compat

---
+++ src/main/java/com/infinityraider/agricraft/plugins/simplyseasons/SimplySeasonsPlugin.java
@@ -0,0 +1,36 @@
+package com.infinityraider.agricraft.plugins.simplyseasons;
+
+import com.infinityraider.agricraft.api.v1.plugin.AgriPlugin;
+import com.infinityraider.agricraft.api.v1.plugin.IAgriPlugin;
+import com.infinityraider.agricraft.api.v1.requirement.AgriSeason;
+import com.infinityraider.agricraft.impl.v1.requirement.SeasonPlugin;
+import com.infinityraider.agricraft.reference.Names;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.ModList;
+
+import java.util.function.BiFunction;
+
+@AgriPlugin
+@SuppressWarnings("unused")
+public class SimplySeasonsPlugin extends SeasonPlugin implements IAgriPlugin {
+    @Override
+    public boolean isEnabled() {
+        return ModList.get().isLoaded(this.getId());
+    }
+
+    @Override
+    public String getId() {
+        return Names.Mods.SIMPLY_SEASONS;
+    }
+
+    @Override
+    public String getName() {
+        return this.getId();
+    }
+
+    @Override
+    protected BiFunction<World, BlockPos, AgriSeason> getSeasonGetter() {
+        return SimplySeasonsSeasonGetter.getInstance();
+    }
+}
