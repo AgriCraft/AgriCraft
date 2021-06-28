a78bff03ff42749176eb98e5ceff2310a80c2743
InfinityRaider <theoneandonlyraider@gmail.com>
2021-06-28 09:55:12 +0100

    Version bump

--- src/main/java/com/infinityraider/agricraft/reference/Reference.java
+++ src/main/java/com/infinityraider/agricraft/reference/Reference.java
@@ -8,9 +8,9 @@ public interface Reference {
 
     String VER_MAJOR = /*^${mod.version_major}^*/ "3";
     String VER_MINOR = /*^${mod.version_minor}^*/ "0";
-    String VER_PATCH = /*^${mod.version_patch}^*/ "3";
-    String MOD_VERSION = /*^${mod.version}^*/ "3.0.3";
-    String VERSION = /*^${mod.version_minecraft}-${mod.version}^*/ "1.16.4-3.0.3";
+    String VER_PATCH = /*^${mod.version_patch}^*/ "4";
+    String MOD_VERSION = /*^${mod.version}^*/ "3.0.4";
+    String VERSION = /*^${mod.version_minecraft}-${mod.version}^*/ "1.16.4-3.0.4";
 
     String VERSION_INFLIB = /*^${mod.version_inflib}^*/ "1.16.4-2.0.0";
     String VERSION_FORGE = /*^${mod.version_forge}^*/ "0.0-0.0.0";
