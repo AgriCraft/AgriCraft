9b97bb7158438e4482291a69c63f45922fc222bb
Ketheroth <h.nicolas2000@gmail.com>
2021-11-12 17:56:45 +0100

    Fix Cyclic & Industrial Foregoing compatibility

--- src/main/java/com/infinityraider/agricraft/plugins/industrialforegoing/IndustrialForegoingCompat.java
+++ src/main/java/com/infinityraider/agricraft/plugins/industrialforegoing/IndustrialForegoingCompat.java
@@ -1,13 +1,13 @@
 package com.infinityraider.agricraft.plugins.industrialforegoing;
 
 import com.buuz135.industrial.api.plant.PlantRecollectable;
-import com.buuz135.industrial.registry.IFRegistries;
-import com.infinityraider.agricraft.AgriCraft;
-import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
+@SuppressWarnings("unused")
 public class IndustrialForegoingCompat {
-    static void execute() {
-        DeferredRegister<PlantRecollectable> register = DeferredRegister.create(IFRegistries.PLANT_RECOLLECTABLES_REGISTRY, AgriCraft.instance.getModId());
-        register.register(AgriCraft.instance.getModId(), AgriPlantRecollectable::new);
+    @SubscribeEvent
+    public static void execute(RegistryEvent.Register<PlantRecollectable> event) {
+        event.getRegistry().register(new AgriPlantRecollectable());
     }
 }
