cd4d297bb1ef6e229d45d140f75e0be507a40f25
InfinityRaider <theoneandonlyraider@gmail.com>
2022-04-08 15:05:27 +0100

    Ported Create compat to 1.18.2

--- src/main/java/com/infinityraider/agricraft/plugins/create/AgriHarvesterMovementBehaviour.java
+++ src/main/java/com/infinityraider/agricraft/plugins/create/AgriHarvesterMovementBehaviour.java
@@ -3,8 +3,8 @@ package com.infinityraider.agricraft.plugins.create;
 import com.infinityraider.agricraft.api.v1.AgriApi;
 import com.simibubi.create.content.contraptions.components.actors.HarvesterMovementBehaviour;
 import com.simibubi.create.content.contraptions.components.structureMovement.MovementContext;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
 
 public class AgriHarvesterMovementBehaviour extends HarvesterMovementBehaviour {
 
@@ -12,8 +12,8 @@ public class AgriHarvesterMovementBehaviour extends HarvesterMovementBehaviour {
 
     @Override
     public void visitNewPosition(MovementContext context, BlockPos pos) {
-        World world = context.world;
-        if (!world.isRemote()) {
+        Level world = context.world;
+        if (!world.isClientSide()) {
             AgriApi.getCrop(world, pos).map(crop -> {
                 if(crop.hasPlant() && crop.isMature()) {
                     crop.harvest(stack -> this.dropItem(context, stack), null);
