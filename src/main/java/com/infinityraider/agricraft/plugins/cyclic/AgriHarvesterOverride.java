310240d736c3d446c5d2ed67da8d107042f835ad
InfinityRaider <theoneandonlyraider@gmail.com>
2022-04-08 15:08:34 +0100

    Ported Cyclic compat to 1.18.2

--- src/main/java/com/infinityraider/agricraft/plugins/cyclic/AgriHarvesterOverride.java
+++ src/main/java/com/infinityraider/agricraft/plugins/cyclic/AgriHarvesterOverride.java
@@ -3,28 +3,29 @@ package com.infinityraider.agricraft.plugins.cyclic;
 import com.infinityraider.agricraft.api.v1.AgriApi;
 import com.infinityraider.agricraft.content.core.BlockCropBase;
 import com.lothrazar.cyclic.api.IHarvesterOverride;
-import net.minecraft.block.BlockState;
-import net.minecraft.entity.item.ItemEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 
 import java.util.function.Consumer;
 
 public class AgriHarvesterOverride implements IHarvesterOverride {
     @Override
-    public boolean appliesTo(BlockState blockState, World world, BlockPos blockPos) {
+    public boolean appliesTo(BlockState blockState, Level world, BlockPos blockPos) {
         return blockState.getBlock() instanceof BlockCropBase;
     }
 
     @Override
-    public boolean attemptHarvest(BlockState blockState, World world, BlockPos blockPos, Consumer<ItemStack> consumer) {
+    public boolean attemptHarvest(BlockState blockState, Level world, BlockPos blockPos, Consumer<ItemStack> consumer) {
         return AgriApi.getCrop(world, blockPos)
                 .map(crop -> crop.harvest(stack -> {
-                        if (!world.isRemote) {
-                            world.addEntity(new ItemEntity(world, blockPos.getX(), blockPos.getY(), blockPos.getZ(), stack));
-                        }
-                    }, null).isSuccess())
+                    if (!world.isClientSide()) {
+                        world.addFreshEntity(new ItemEntity(world, blockPos.getX(), blockPos.getY(), blockPos.getZ(), stack));
+                    }
+                }, null) == InteractionResult.SUCCESS)
                 .orElse(false);
     }
 }
