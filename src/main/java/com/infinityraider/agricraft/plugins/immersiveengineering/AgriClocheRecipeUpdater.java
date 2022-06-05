b131992a686525e9f1ede64601eb9783696efe00
InfinityRaider <theoneandonlyraider@gmail.com>
2022-06-05 22:14:02 +0100

    Code cleanup

--- src/main/java/com/infinityraider/agricraft/plugins/immersiveengineering/AgriClocheRecipeUpdater.java
+++ src/main/java/com/infinityraider/agricraft/plugins/immersiveengineering/AgriClocheRecipeUpdater.java
@@ -3,7 +3,7 @@ package com.infinityraider.agricraft.plugins.immersiveengineering;
 import blusunrize.immersiveengineering.api.crafting.ClocheRecipe;
 import com.infinityraider.agricraft.AgriCraft;
 import com.infinityraider.agricraft.api.v1.event.AgriRegistryEvent;
-import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.world.item.crafting.RecipeManager;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class AgriClocheRecipeUpdater {
@@ -18,17 +18,14 @@ public class AgriClocheRecipeUpdater {
     @SubscribeEvent
     @SuppressWarnings("unused")
     public void onPlantsInitialized(AgriRegistryEvent.Initialized.Plant event) {
-        if(event.getSide().isServer()) {
-            AgriCraft.instance.getMinecraftServer().getRecipeManager().getAllRecipesFor(ClocheRecipe.TYPE).stream()
+        // fetch recipe manager
+        RecipeManager recipeManager = event.getSide().isServer()
+                ? AgriCraft.instance.getMinecraftServer().getRecipeManager()
+                : AgriCraft.instance.getClientWorld().getRecipeManager();
+        // update cloche outputs
+        recipeManager.getAllRecipesFor(ClocheRecipe.TYPE).stream()
                     .filter(recipe -> recipe instanceof AgriClocheRecipe)
                     .map(recipe -> (AgriClocheRecipe) recipe)
                     .forEach(AgriClocheRecipe::updateOutputs);
-        } else {
-            ClientLevel world = (ClientLevel) AgriCraft.instance.getClientWorld();
-            world.getRecipeManager().getAllRecipesFor(ClocheRecipe.TYPE).stream()
-                    .filter(recipe -> recipe instanceof AgriClocheRecipe)
-                    .map(recipe -> (AgriClocheRecipe) recipe)
-                    .forEach(AgriClocheRecipe::updateOutputs);
-        }
     }
 }
