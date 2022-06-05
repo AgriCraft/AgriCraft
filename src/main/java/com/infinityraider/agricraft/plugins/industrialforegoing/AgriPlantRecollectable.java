a051ebb0af72257214170cce114c5b3c2e2e6c90
InfinityRaider <theoneandonlyraider@gmail.com>
2022-06-05 23:26:22 +0100

    Fix Industrial Foregoing Compat crash

--- src/main/java/com/infinityraider/agricraft/plugins/industrialforegoing/AgriPlantRecollectable.java
+++ src/main/java/com/infinityraider/agricraft/plugins/industrialforegoing/AgriPlantRecollectable.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class AgriPlantRecollectable extends PlantRecollectable {
     protected AgriPlantRecollectable() {
         super(AgriCraft.instance.getModId());
+        this.setRegistryName(AgriCraft.instance.getModId(), "plant_recollectable");
     }
 
     @Override
