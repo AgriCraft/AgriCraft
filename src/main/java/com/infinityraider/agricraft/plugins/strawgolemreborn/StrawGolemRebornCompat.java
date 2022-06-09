b8c10a948528a06bf1dec805fb6cc5c6a02819ca
InfinityRaider <theoneandonlyraider@gmail.com>
2022-06-09 22:25:33 +0100

    Add Straw Golem Reborn Compat

--- src/main/java/com/infinityraider/agricraft/plugins/strawgolemreborn/StrawGolemRebornCompat.java
+++ src/main/java/com/infinityraider/agricraft/plugins/strawgolemreborn/StrawGolemRebornCompat.java
@@ -1,7 +1,39 @@
 package com.infinityraider.agricraft.plugins.strawgolemreborn;
 
+import com.google.common.collect.Lists;
+import com.infinityraider.agricraft.content.AgriTileRegistry;
+import com.infinityraider.agricraft.content.core.TileEntityCrop;
+import com.t2pellet.strawgolem.crop.CropRegistry;
+import com.t2pellet.strawgolem.entity.EntityStrawGolem;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+import java.util.List;
+
 class StrawGolemRebornCompat {
     static void init() {
+        AgriCropLogic logic = new AgriCropLogic();
+        AgriTileRegistry.getInstance().crop.onRegistration(t -> CropRegistry.INSTANCE.register(t, logic, logic, logic));
+    }
 
+    private static class AgriCropLogic implements CropRegistry.IHarvestChecker<TileEntityCrop>, CropRegistry.IHarvestLogic<TileEntityCrop>, CropRegistry.IReplantLogic<TileEntityCrop> {
+        @Override
+        public boolean isMature(TileEntityCrop crop) {
+            return crop.isMature();
+        }
+
+        @Override
+        public List<ItemStack> doHarvest(ServerLevel level, EntityStrawGolem golem, BlockPos pos, TileEntityCrop crop) {
+            List<ItemStack> drops = Lists.newArrayList();
+            crop.harvest(drops::add, golem);
+            return drops;
+        }
+
+        @Override
+        public void doReplant(Level level, BlockPos pos, TileEntityCrop crop) {
+            // do nothing
+        }
     }
 }
--- src/main/java/com/infinityraider/agricraft/plugins/strawgolemreborn/StrawGolemRebornPlugin.java
+++ src/main/java/com/infinityraider/agricraft/plugins/strawgolemreborn/StrawGolemRebornPlugin.java
@@ -4,11 +4,21 @@ import com.infinityraider.agricraft.AgriCraft;
 import com.infinityraider.agricraft.api.v1.plugin.AgriPlugin;
 import com.infinityraider.agricraft.api.v1.plugin.IAgriPlugin;
 import com.infinityraider.agricraft.reference.Names;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 @SuppressWarnings("unused")
 @AgriPlugin(modId = Names.Mods.STRAW_GOLEM)
 public class StrawGolemRebornPlugin implements IAgriPlugin {
+    @SuppressWarnings("unused")
+    public StrawGolemRebornPlugin() {
+        // Register agricraft crops as a crop to harvest
+        try {
+            StrawGolemRebornCompat.init();
+        } catch(Exception e) {
+            AgriCraft.instance.getLogger().error("Failed to initialize Straw Golem Reborn Compat, notify the mod author");
+            AgriCraft.instance.getLogger().printStackTrace(e);
+        }
+    }
+
     @Override
     public boolean isEnabled() {
         return AgriCraft.instance.getConfig().enableStrawGolemRebornCompat();
@@ -22,16 +32,5 @@ public class StrawGolemRebornPlugin implements IAgriPlugin {
     @Override
     public String getDescription() {
         return "Straw Golem Reborn compatibility";
-    }
-
-    @Override
-    public void onCommonSetupEvent(FMLCommonSetupEvent event) {
-        // Register agricraft crops as a crop to harvest
-        try {
-            StrawGolemRebornCompat.init();
-        } catch(Exception e) {
-            AgriCraft.instance.getLogger().error("Failed to initialize Straw Golem Reborn Compat, notify the mod author");
-            AgriCraft.instance.getLogger().printStackTrace(e);
-        }
     }
 }
