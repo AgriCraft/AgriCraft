ac8f6b0a0cd72dd79304ece5c8966dc5c334edf6
Ketheroth <ketheroth@protonmail.com>
2023-06-18 17:00:23 +0200

    fix crash with create 0.5.1b (fixes #1507)

--- src/main/java/com/infinityraider/agricraft/plugins/create/AgriHarvesterMovementBehaviour.java
+++ src/main/java/com/infinityraider/agricraft/plugins/create/AgriHarvesterMovementBehaviour.java
@@ -1,8 +1,8 @@
 package com.infinityraider.agricraft.plugins.create;
 
 import com.infinityraider.agricraft.api.v1.AgriApi;
-import com.simibubi.create.content.contraptions.components.actors.HarvesterMovementBehaviour;
-import com.simibubi.create.content.contraptions.components.structureMovement.MovementContext;
+import com.simibubi.create.content.contraptions.actors.harvester.HarvesterMovementBehaviour;
+import com.simibubi.create.content.contraptions.behaviour.MovementContext;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 
