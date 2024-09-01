a563aa6ec07a235b113fb9e128bced4ff11345f1
unilock <unilock@fennet.rentals>
2024-09-01 13:33:10 -0400

    Fix Create compatibility
    Mixin necessary :/
    
    Signed-off-by: unilock <unilock@fennet.rentals>

--- common/src/main/java/com/agricraft/agricraft/plugin/create/CreatePlugin.java
+++ common/src/main/java/com/agricraft/agricraft/plugin/create/CreatePlugin.java
@@ -1,18 +1,11 @@
 package com.agricraft.agricraft.plugin.create;
 
-import com.simibubi.create.AllMovementBehaviours;
-import net.minecraft.resources.ResourceLocation;
-
 public class CreatePlugin {
 
-    /*
-     * Create does not really provide a way to implement compatibility for more complex crops.
-     * Therefore, in order to obtain compatibility, we must employ a rather aggressive approach,
-     * which consists of overriding their default harvesting behaviour with our own.
-     * This can be toggled in the config, so if people do not like this, they can disable it.
-     * Until Create exposes harvesting logic in an API or via IMC, we must do it this way, unfortunately.
+    /**
+     * @see com.agricraft.agricraft.mixin.compat.create.HarvesterMovementBehaviourMixin
      */
     public static void init() {
-        AllMovementBehaviours.registerBehaviour(new ResourceLocation("create", "mechanical_harvester"), new AgriHarvesterMovementBehaviour());
+        //AllMovementBehaviours.registerBehaviour(AllBlocks.MECHANICAL_HARVESTER.get(), new AgriHarvesterMovementBehaviour());
     }
 }
